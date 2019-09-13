7f987c2168db243107b2e0a96373a5d06729da3f
pieromarini <piero.marini21@gmail.com>
2019-09-13 11:31:49 -0500

    Added singleton to ChocolateBoiler

--- ChocolateBoiler.java
+++ ChocolateBoiler.java
@@ -1,38 +1,48 @@
 public class ChocolateBoiler {
-    private boolean empty;
-    private boolean boiled;
+  private boolean empty;
+  private boolean boiled;
 
-    public ChocolateBoiler() {
-        empty = true;
-        boiled = false;
-    }
+  // Singleton
+  private static ChocolateBoiler instance;
 
-    public void fill(){
-        if(isEmpty()){
-            empty = false;
-            boiled = false;
-        }
+  public static ChocolateBoiler getInstance() {
+    if (instance == null) {
+      instance = new ChocolateBoiler();
     }
+    return instance;
+  }
 
-    public void drain(){
-        if(isEmpty() && isBoiled()){
-            //drain the boiled milk and chocolate
-            empty = true;
-        }
-    }
+  private ChocolateBoiler() {
+    empty = true;
+    boiled = false;
+  }
 
-    public void boil(){
-        if(!isEmpty() && isBoiled()){
-            //bring the contents to a boil
-            boiled = true;
-        }
+  public void fill() {
+    if (isEmpty()) {
+      empty = false;
+      boiled = false;
     }
+  }
 
-    public boolean isEmpty(){
-        return empty;
+  public void drain() {
+    if (isEmpty() && isBoiled()) {
+      // drain the boiled milk and chocolate
+      empty = true;
     }
+  }
 
-    public boolean isBoiled(){
-        return boiled;
+  public void boil() {
+    if (!isEmpty() && isBoiled()) {
+      // bring the contents to a boil
+      boiled = true;
     }
+  }
+
+  public boolean isEmpty() {
+    return empty;
+  }
+
+  public boolean isBoiled() {
+    return boiled;
+  }
 }
